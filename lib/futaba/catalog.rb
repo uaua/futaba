b202f34b4754b0dc4505c6a7b3674d9db73241b3
Kosuke Asami <tfortress58@gmail.com>
2013-07-07 18:07:25 +0900

    Provide a method that fetches a catalog from web and parses it

--- lib/futaba/catalog.rb
+++ lib/futaba/catalog.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "nokogiri"
+
 module Futaba
   class Catalog
     class << self
@@ -6,10 +9,49 @@ module Futaba
       end
     end
 
-    attr_reader :uri
+    attr_reader :threads, :uri
 
     def initialize(board_uri)
+      @board_uri = board_uri
       @uri = Catalog.catalog_uri(board_uri)
     end
+
+    def fetch
+      open(@uri) do |document|
+        parsed_document = Nokogiri::HTML(document)
+        @threads = extract_threads(parsed_document)
+      end
+    end
+
+    private
+    def extract_threads(parsed_document)
+      threads = []
+      parsed_document.xpath('//table[@align="center"]/tr').each do |tr|
+        tr.xpath('td').each do |td|
+          threads << extract_thread(td)
+        end
+      end
+      threads
+    end
+
+    def extract_thread(parsed_td)
+      uri = @board_uri + parsed_td.at("a")["href"]
+      head_letters = parsed_td.at("small").text if parsed_td.at("small")
+      thumbnail_uri = parsed_td.at("a").at("img")["src"]
+      n_posts = parsed_td.at("font").text.to_i
+
+      Futaba::Thread.new(
+        uri,
+        head_letters,
+        thumbnail_uri,
+        n_posts)
+    end
   end
+
+  Thread = Struct.new(
+    :uri,
+    :head_letters,
+    :thumbnail_uri,
+    :n_posts
+  )
 end
--- test/test-catalog.rb
+++ test/test-catalog.rb
@@ -1,10 +1,52 @@
+# -*- coding: utf-8 -*-
 require "futaba"
+require "test/unit/rr"
 
 class TestCatalog < Test::Unit::TestCase
   class TestURI < self
     def test_uri
       catalog = Futaba::Catalog.new("http://may.2chan.net/b/")
       assert_equal("http://may.2chan.net/b/futaba.php?mode=cat", catalog.uri)
+    end
+  end
+
+  class TestThreads < self
+    def test_threads
+      catalog = Futaba::Catalog.new("http://may.2chan.net/b/")
+      document = File.read(fixture_path("catalog.html"), :encoding => Encoding::Shift_JIS)
+      stub(catalog).open(catalog.uri).yields(document)
+
+      catalog.fetch
+
+      assert_equal(
+        [
+          {:head_letters=>nil,
+            :n_posts=>11,
+            :thumbnail_uri=>"http://hoge/fuga.jpg",
+            :uri=>"http://may.2chan.net/b/res/0.htm"},
+          {:head_letters=>"ぜかまし",
+            :n_posts=>22,
+            :thumbnail_uri=>"http://akagi/simakaze.jpg",
+            :uri=>"http://may.2chan.net/b/res/1.htm"},
+          {:head_letters=>nil,
+            :n_posts=>33,
+            :thumbnail_uri=>"http://foo/bar.png",
+            :uri=>"http://may.2chan.net/b/res/2.htm"}
+        ],
+        catalog.threads.collect {|thread|
+          {
+            :uri => thread.uri,
+            :head_letters => thread.head_letters,
+            :thumbnail_uri => thread.thumbnail_uri,
+            :n_posts => thread.n_posts,
+          }
+        }
+      )
+    end
+
+    private
+    def fixture_path(basename)
+      File.join(__dir__, 'fixtures', basename)
     end
   end
 end
