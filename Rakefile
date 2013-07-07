a1ca9c950f03ae92d0365f86b8eea6da6a6dda07
Kosuke Asami <tfortress58@gmail.com>
2013-07-07 13:47:35 +0900

    test: Implement tests skeleton

--- Rakefile
+++ Rakefile
@@ -1,1 +1,8 @@
+task :default => :test
+
 require "bundler/gem_tasks"
+
+desc "Run tests"
+task :test do
+  ruby("test/run-test.rb")
+end
---
+++ test/run-test.rb
@@ -0,0 +1,13 @@
+$VERBOSE = true
+
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+
+lib_dir = File.join(base_dir, "lib")
+test_dir = File.join(base_dir, "test")
+
+$LOAD_PATH.unshift(lib_dir)
+
+require "test-unit"
+
+Thread.abort_on_exception = true
+exit Test::Unit::AutoRunner.run(true, test_dir)
