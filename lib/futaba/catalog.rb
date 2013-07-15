c056e5ddef33cdc4ae1a69e3d1e0509ed4cabfe5
Kosuke Asami <tfortress58@gmail.com>
2013-07-15 18:45:22 +0900

    catalog: Fetch a catalog by specifying the number of threads

--- lib/futaba/catalog.rb
+++ lib/futaba/catalog.rb
@@ -11,13 +11,15 @@ module Futaba
 
     attr_reader :threads, :uri
 
+    MAX_CATALOG_SIZE = { :x => 999, :y => 999 }
+
     def initialize(board_uri)
       @board_uri = board_uri
       @uri = Catalog.catalog_uri(board_uri)
     end
 
-    def fetch(order_type: :default, n_letters: 0)
-      uri = make_fetch_uri(order_type, n_letters)
+    def fetch(order_type: :default, n_letters: 0, n_threads: -1)
+      uri = make_fetch_uri(order_type, n_letters, n_threads)
 
       open(uri) do |document|
         parsed_document = Nokogiri::HTML(document)
@@ -49,7 +51,7 @@ module Futaba
         n_posts)
     end
 
-    def make_fetch_uri(order_type, n_letters)
+    def make_fetch_uri(order_type, n_letters, n_threads)
       uri = @uri
       case order_type
       when :newer
@@ -61,7 +63,10 @@ module Futaba
       when :decreasing
         uri += "&sort=4"
       end
-      uri += "&cxyl=10x10x#{n_letters}"
+      max_p = n_threads < 0
+      x = MAX_CATALOG_SIZE[:x]
+      y = max_p ? MAX_CATALOG_SIZE[:y] : (n_threads / x.to_f).ceil
+      uri += "&cxyl=#{x}x#{y}x#{n_letters}"
     end
   end
 end
