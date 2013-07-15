42017815a30f8ca608f2416e9c789fb29e86792c
Kosuke Asami <tfortress58@gmail.com>
2013-07-16 00:02:01 +0900

    Promote Thread from Struct to Class

--- lib/futaba/thread.rb
+++ lib/futaba/thread.rb
@@ -1,9 +1,12 @@
 module Futaba
-  class Thread < Struct.new(
-      :uri,
-      :head_letters,
-      :thumbnail_uri,
-      :n_posts
-  )
+  class Thread
+    attr_reader :uri, :head_letters, :thumbnail_uri, :n_posts
+
+    def initialize(uri, head_letters, thumbnail_uri, n_posts)
+      @uri = uri
+      @head_letters = head_letters
+      @thumbnail_uri = thumbnail_uri
+      @n_posts = n_posts
+    end
   end
 end
